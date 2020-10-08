86898171845b316baf0c2812952b566cad7a5110
blazerroad <blazer.road@gmail.com>
2020-10-08 15:47:12 -0400

    resovler

--- repository.ts
+++ repository.ts
@@ -1,4 +1,4 @@
-import { BaseFetchParam, azurefetch } from 'react-native-azure-cosmos/azurecosmos'
+import { BaseFetchParam, azurefetch, SpFetchParam, UpdateFetchParam, BaseFetchParamDefualt, SpFetchParamDefault, UpdateFetchParamDefault } from 'react-native-azure-cosmos/azurecosmos'
 import { FetchParam, azuregermlinfetch } from 'react-native-azure-cosmos-gremlin/germlin'
 
 export interface IRepository<TRequest> {
@@ -34,5 +34,89 @@ export class BaseSerivce<TRequest, TRepository extends IRepository<TRequest>> im
     fetch(param: TRequest): Promise<any> {
         return this.repository.fetch(param);
     }
+}
+
+export class BaseReduxParam {
+    dispatch: any;
+    state: any;
+    constructor(dispatch: any, state: any) {
+        this.dispatch = dispatch;
+        this.state = state;
+    }
+}
+
+
+export class BaseFetchReduxService<TRequest extends BaseFetchParam, TRepository extends IRepository<TRequest>> extends BaseSerivce<TRequest, TRepository>
+{
+    constructor(repository: TRepository) {
+        super(repository);
+    }
+    async  rfetch(param: TRequest, reduxParam: BaseReduxParam): Promise<any> {
+        try {
+            const response = await super.fetch(param);
+            if (response.status === 304) {
+                return;
+            }
+            if (!response.ok) {
+                throw new Error("There is some thing wrong from API");
+            }
+            const resData = await response.json();
+            reduxParam.dispatch({ type: param.actionName, data: resData })
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+}
+
+
+
+export class DefaultAzureFetchService extends BaseFetchReduxService<BaseFetchParam, AzurefetchRepository>
+{
+    constructor() {
+        super(new AzurefetchRepository());
+
+    }
+}
+
+export class DefaultAzureFetchSpService extends BaseFetchReduxService<SpFetchParam, AzurefetchRepository>
+{
+    constructor() {
+        super(new AzurefetchRepository());
+    }
+}
+
+export class DefaultAzureFetchUpdateService extends BaseFetchReduxService<UpdateFetchParam, AzurefetchRepository>
+{
+    constructor() {
+        super(new AzurefetchRepository());
+    }
+}
+
+export class DefaultAzureGermlinFetchService extends BaseSerivce<FetchParam, AzureGermlinfetchRepository>
+{
+    constructor() {
+        super(new AzureGermlinfetchRepository());
+    }
+    async  rfetch(param: FetchParam, reduxParam: BaseReduxParam): Promise<any> {
+        try {
+            const response = await super.fetch(param);
+            if (response.status === 304) {
+                return ;
+            }
+            if (!response.ok) {
+                throw new Error("There is some thing wrong from API");
+            }
+            const resData = await response.json();
+            reduxParam.dispatch({ type: param.actionName, data: resData })
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+}
+
+const testFunc = () => {
+   
 }
 
